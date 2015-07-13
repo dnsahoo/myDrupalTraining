317e7d824954cbf1a7c61b4bc0fbd41a521edfe4
unknown <dhirendra.sahoo@HYD1-DHP-30035.synapse.com>
2015-07-13 15:09:51 +0530

    my files

---
+++ sites/all/themes/dgd7/template.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Returns HTML for a "more" link, like those used in blocks.
+ *
+ * @param $variables
+ * An associative array containing:
+ * - url: The url of the main page.
+ * - title: A descriptive verb for the link, like 'Read more'.
+ */
+function dgd7_more_link($variables) {
+ return '<div class="more-link">' . l(t('More'), $variables['url'], array('attributes' => array('title' => $variables['title']))) . '</div>';
+}
